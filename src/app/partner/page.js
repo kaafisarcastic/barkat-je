20a11b33baa4dbc6d275621ee182740cf40bd0be
Mohd Saad <saadsiddiqui468@gmail.com>
2025-07-17 16:22:19 +0530

    suspense tag added

--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 
@@ -213,119 +213,126 @@ export default function QuestionnairePagePartner() {
   const progress = Math.round(((currentIndex + 1) / questions.length) * 100);
 
   return (
-    <section className="rounded-2xl bg-[#ffffff] w-full py-12">
-      <div className="max-w-6xl mx-auto px-6 py-8">
-        <h1 className="text-black font-sanserif text-5xl md:text-7xl leading-tight">
-          Know Your Partner Before Marriage
-        </h1>
-        <p className="text-[#555555] font-sanserif text-base mt-4">
-          Welcome to the compatibility test. Tool that helps couples explore
-          core areas of their relationship before marriage.
-        </p>
+    <Suspense>
+      <section className="rounded-2xl bg-[#ffffff] w-full py-12">
+        <div className="max-w-6xl mx-auto px-6 py-8">
+          <h1 className="text-black font-sanserif text-5xl md:text-7xl leading-tight">
+            Know Your Partner Before Marriage
+          </h1>
+          <p className="text-[#555555] font-sanserif text-base mt-4">
+            Welcome to the compatibility test. Tool that helps couples explore
+            core areas of their relationship before marriage.
+          </p>
 
-        <div className="mt-12 bg-white/20 backdrop-blur-md p-6 rounded-xl shadow border border-white/30">
-          {/* Top Bar */}
-          <div className="flex justify-between items-center mb-4">
-            <span className="text-sm text-gray-600">
-              Question {currentIndex + 1} of {questions.length}
-            </span>
-            <span className="text-sm bg-[#066006] text-white px-3 py-1 rounded-full">
-              {progress}% Complete
-            </span>
-          </div>
-
-          {/* Question */}
-          <h2 className="text-xl font-semibold mb-4 text-black">
-            {currentQuestion.question_text}
-          </h2>
-
-          {/* MCQ */}
-          {hasOptions && (
-            <div className="space-y-2 mb-4">
-              {currentQuestion.options.map((opt, idx) => (
-                <label key={idx} className="flex items-center gap-2 text-black">
-                  <input
-                    type="radio"
-                    name={`q-${currentQuestion.id}`}
-                    value={opt}
-                    checked={qResponse.selected_option === opt}
-                    onChange={() => handleOptionChange(currentQuestion.id, opt)}
-                  />
-                  {opt}
-                </label>
-              ))}
+          <div className="mt-12 bg-white/20 backdrop-blur-md p-6 rounded-xl shadow border border-white/30">
+            {/* Top Bar */}
+            <div className="flex justify-between items-center mb-4">
+              <span className="text-sm text-gray-600">
+                Question {currentIndex + 1} of {questions.length}
+              </span>
+              <span className="text-sm bg-[#066006] text-white px-3 py-1 rounded-full">
+                {progress}% Complete
+              </span>
             </div>
-          )}
 
-          {/* Explanation */}
-          {hasOptions && qResponse.selected_option && (
-            <div className="mb-4">
-              <label className="block text-sm text-gray-600 mb-1">
-                Please elaborate on why you chose this:
-              </label>
-              <textarea
-                className="w-full border rounded px-3 py-2 text-black bg-[#f9f9f9]"
-                rows={4}
-                value={qResponse.explanation || ""}
-                onChange={(e) =>
-                  handleExplanationChange(currentQuestion.id, e.target.value)
-                }
-                required
-              />
-              <p className="text-sm text-gray-500 mt-1">
-                Minimum 20 and maximum 100 characters
-              </p>
-            </div>
-          )}
+            {/* Question */}
+            <h2 className="text-xl font-semibold mb-4 text-black">
+              {currentQuestion.question_text}
+            </h2>
 
-          {/* Text-only question */}
-          {!hasOptions && (
-            <div className="mb-4">
-              <label className="block text-sm text-gray-600 mb-1">
-                Your Answer:
-              </label>
-              <textarea
-                className="w-full border rounded px-3 py-2 text-black bg-[#f9f9f9]"
-                rows={5}
-                value={qResponse.explanation || ""}
-                onChange={(e) =>
-                  handleExplanationChange(currentQuestion.id, e.target.value)
-                }
-                required
-              />
-              <p className="text-sm text-gray-500 mt-1">
-                Minimum 100 and maximum 400 characters
-              </p>
-            </div>
-          )}
-
-          {/* Navigation */}
-          <div className="flex justify-between pt-4">
-            <button
-              onClick={handleBack}
-              disabled={currentIndex === 0}
-              className="px-6 py-3 bg-white/20 backdrop-blur-md text-black rounded-md shadow border border-white/30 hover:bg-[#e94e4e] transition"
-            >
-              Back
-            </button>
-            {currentIndex < questions.length - 1 ? (
-              <button
-                onClick={handleNext}
-                className="px-6 py-3 bg-black text-white rounded-md shadow hover:bg-[#e94e4e] transition"
-              >
-                Next
-              </button>
-            ) : (
-              <button
-                onClick={handleSubmit}
-                className="px-6 py-3 bg-black text-white rounded-md shadow hover:bg-[#e94e4e] transition"
-              >
-                Submit
-              </button>
+            {/* MCQ */}
+            {hasOptions && (
+              <div className="space-y-2 mb-4">
+                {currentQuestion.options.map((opt, idx) => (
+                  <label
+                    key={idx}
+                    className="flex items-center gap-2 text-black"
+                  >
+                    <input
+                      type="radio"
+                      name={`q-${currentQuestion.id}`}
+                      value={opt}
+                      checked={qResponse.selected_option === opt}
+                      onChange={() =>
+                        handleOptionChange(currentQuestion.id, opt)
+                      }
+                    />
+                    {opt}
+                  </label>
+                ))}
+              </div>
             )}
+
+            {/* Explanation */}
+            {hasOptions && qResponse.selected_option && (
+              <div className="mb-4">
+                <label className="block text-sm text-gray-600 mb-1">
+                  Please elaborate on why you chose this:
+                </label>
+                <textarea
+                  className="w-full border rounded px-3 py-2 text-black bg-[#f9f9f9]"
+                  rows={4}
+                  value={qResponse.explanation || ""}
+                  onChange={(e) =>
+                    handleExplanationChange(currentQuestion.id, e.target.value)
+                  }
+                  required
+                />
+                <p className="text-sm text-gray-500 mt-1">
+                  Minimum 20 and maximum 100 characters
+                </p>
+              </div>
+            )}
+
+            {/* Text-only question */}
+            {!hasOptions && (
+              <div className="mb-4">
+                <label className="block text-sm text-gray-600 mb-1">
+                  Your Answer:
+                </label>
+                <textarea
+                  className="w-full border rounded px-3 py-2 text-black bg-[#f9f9f9]"
+                  rows={5}
+                  value={qResponse.explanation || ""}
+                  onChange={(e) =>
+                    handleExplanationChange(currentQuestion.id, e.target.value)
+                  }
+                  required
+                />
+                <p className="text-sm text-gray-500 mt-1">
+                  Minimum 100 and maximum 400 characters
+                </p>
+              </div>
+            )}
+
+            {/* Navigation */}
+            <div className="flex justify-between pt-4">
+              <button
+                onClick={handleBack}
+                disabled={currentIndex === 0}
+                className="px-6 py-3 bg-white/20 backdrop-blur-md text-black rounded-md shadow border border-white/30 hover:bg-[#e94e4e] transition"
+              >
+                Back
+              </button>
+              {currentIndex < questions.length - 1 ? (
+                <button
+                  onClick={handleNext}
+                  className="px-6 py-3 bg-black text-white rounded-md shadow hover:bg-[#e94e4e] transition"
+                >
+                  Next
+                </button>
+              ) : (
+                <button
+                  onClick={handleSubmit}
+                  className="px-6 py-3 bg-black text-white rounded-md shadow hover:bg-[#e94e4e] transition"
+                >
+                  Submit
+                </button>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Suspense>
   );
 }
